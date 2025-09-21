83903119be8f888efb781c6d49839e4c355cff79
Goodness C Igwe <igwegoodnessifeoma@gmail.com>
2025-09-21 20:53:12 +0100

    modify register.php and change index.php path for deployment

--- public/register.php
+++ public/register.php
@@ -86,10 +86,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <a class="navbar-brand" href="index.php">Exam System</a>
+            <a class="navbar-brand" href="#">Exam System</a>
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../index.php">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
                 </ul>
             </div>
